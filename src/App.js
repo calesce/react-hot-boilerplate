89c39ffe617fc8aab647a0089d085905c1a76536
Cale Newman <newman.cale@gmail.com>
2016-10-02 17:48:59 -0500

    convert WDS example to express

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
 
-export default class App extends Component {
+export default class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { counter: 0 };
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(this.tick.bind(this), 1000);
+  }
+
+  tick() {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
   render() {
     return (
-      <h1>Hello, world.</h1>
-    );
+      <h2>Counter: {this.state.counter}</h2>
+   );
   }
 }
